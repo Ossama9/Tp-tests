e081998790c16697b01595b65486d5988013b6ff
Ossama9 <oussama.dahbi98@gmail.com>
2023-05-23 18:29:39 +0200

    implement todolist entity

---
+++ Entities/ToDoList.js
@@ -0,0 +1,21 @@
+class ToDoList {
+    constructor(user) {
+        this.user = user;
+        this.items = [];
+        this.lastItemCreationDate = null;
+    }
+
+    canAddItem() {
+
+    }
+
+    add(item) {
+        if (this.isValidUser() && this.canAddItem()) {
+            this.items.push(item);
+            this.lastItemCreationDate = item.creationDate;
+            console.log('Item ajouté à la ToDoList.');
+        } else {
+            console.log('Impossible d\'ajouter l\'item à la ToDoList.');
+        }
+    }
+}
