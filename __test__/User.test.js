f16e093e20e2685fd734f2d2e31bd5d7e42428db
Nfassory Diaby <fassory.diaby@gmail.com>
2023-05-24 17:20:29 +0200

    user test

--- __test__/User.test.js
+++ __test__/User.test.js
@@ -12,8 +12,35 @@ describe('User', () => {
         );
     });
 
-    it('Verification email', () => {
+    it('Test email', () => {
         const res = isValidEmail();
+        expect(res).toBe(true);
+    });
+
+
+    it('Test Lastname', () => {
+        const res = isValidLastname();
+        expect(res).toBe(true);
+    });
+
+    it('Test Firstname', () => {
+        const res = isValidFirstname();
+        expect(res).toBe(true);
+    });
+
+    it('Test Password', () => {
+        const res = isValidPassword();
+        expect(res).toBe(true);
+    });
+
+
+    it('Test Age', () => {
+        const res = isValidAge();
+        expect(res).toBe(true);
+    });
+
+    it('Test User', () => {
+        const res = isValidUser();
         expect(res).toBe(true);
     });
 
