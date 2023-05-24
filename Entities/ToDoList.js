f0bafd04a93fd5871f7b1078d043e6308c35081b
Ossama9 <oussama.dahbi98@gmail.com>
2023-05-24 19:20:49 +0200

    implement todolist entity

--- Entities/ToDoList.js
+++ Entities/ToDoList.js
@@ -1,12 +1,31 @@
 class ToDoList {
+    /**
+     * @param {object} user - L'objet utilisateur associé à l'instance.
+     * @property {object} user - L'objet utilisateur associé à l'instance.
+     * @property {Array} items - Un tableau vide pour stocker les objets d'éléments.
+     * @property {Date|null} lastItemCreationDate - La date et l'heure de création du dernier élément, ou null s'il n'y a pas encore eu de création d'élément.
+     */
     constructor(user) {
         this.user = user;
         this.items = [];
         this.lastItemCreationDate = null;
     }
 
+    isItemNameUnique(newItemName: string) {
+        const condition = (item) => item.name === newItemName
+        this.items.some()
+    }
+
     canAddItem() {
-        
+        const date_now = new Date();
+        const limit_date = new Date(date_now.getTime() - (30 * 60000));
+        if (this.lastItemCreationDate < limit_date) {
+            console.log("ok")
+        } else {
+            console.log("non")
+        }
+        this.items.forEach()
+
     }
 
     add(item) {
