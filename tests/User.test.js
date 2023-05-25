036529c743741355cb76e7943a8fa894ecf66aaa
Ayman-BEDDA <bedda.ayman.sio@gmail.com>
2023-05-25 11:51:39 +0200

    tests user and fix user entity's method

--- tests/User.test.js
+++ tests/User.test.js
@@ -12,68 +12,77 @@ describe('User', () => {
         );
     });
 
+    //Test valid email
     it('Test email valide', () => {
         const res = user.isValidEmail();
         expect(res).toBe(true);
     });
-
     it('Test email non valide', () => {
         user.email = "Johndoeexample.com";
         const res = user.isValidEmail();
         expect(res).toBe(false);
     });
 
+    //Test lastName
     it('Test Lastname Valide', () => {
         const res = user.isValidLastname();
         expect(res).toBe(true);
     });
-
     it('Test Lastname non Valide', () => {
-        user.lastname = null;
+        user.lastname = '';
         const res = user.isValidLastname();
         expect(res).toBe(false);
     });
 
+    //Test firstName
     it('Test Firstname Valide', () => {
         const res = user.isValidFirstname();
         expect(res).toBe(true);
     });
-
     it('Test Firstname non Valide', () => {
         user.firstname = null;
         const res = user.isValidFirstname();
         expect(res).toBe(false);
     });
 
+    //Test password
     it('Test Password Valide', () => {
         const res = user.isValidPassword();
         expect(res).toBe(true);
     });
-
-    it('Test Password non Valide', () => {
+    it('Test not strong password', () => {
         user.password = "test"
         const res = user.isValidPassword();
         expect(res).toBe(false);
     });
+    
 
+    //Test valid age
     it('Test Age Valide', () => {
         const res = user.isValidAge();
         expect(res).toBe(true);
     });
-
     it('Test Age non Valide', () => {
         user.dateOfBirth = '01/01/2020';
         const res = user.isValidAge();
         expect(res).toBe(false);
     });
 
+    //User valid
     it('Test User Valide', () => {
         const res = user.isValidUser();
         expect(res).toBe(true);
     });
-
     it('Test User non Valide', () => {
         user.dateOfBirth = '01/01/2020';
+        const res = user.isValidUser();
+        expect(res).toBe(false);
+    });
+
+    //Test empty field
+    it('Test if one or more fields are empty', () => {
+        user.dateOfBirth = '';
+        user.lastname = '';
         const res = user.isValidUser();
         expect(res).toBe(false);
     });
